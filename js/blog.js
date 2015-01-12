588a18c33e174f882390a39aa94f1ea3465049d3
yangzhyo <yangzhyo@gmail.com>
2015-01-13 01:00:23 +0800

    add category and tag cloud

--- js/blog.js
+++ js/blog.js
@@ -3,17 +3,15 @@ $(function(){
     tags.each(function(){
         $(this).addClass("post-tag-"+Math.floor((Math.random() * 4) + 1));
     });
-    /*
+    
     $(window).scroll(function() {
         if($(window).scrollTop() >= 100){
-            $('.topfade').fadeIn(300);
+            $('.go-to-top').fadeIn(300);
         }else{
-            $('.topfade').fadeOut(300);
+            $('.go-to-top').fadeOut(300);
         }
     });
 
-    $('.topfade').click(function(){
+    $('.go-to-top').click(function(){
         $('html,body').animate({scrollTop: '0px'}, 800);});
-
-*/
 });
